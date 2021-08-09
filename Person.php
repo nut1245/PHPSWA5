f1e8df5c24ffb1488f2aeed29f9cf2f086415d0d
nut1245 <73591319+nut1245@users.noreply.github.com>
2021-08-09 16:52:46 +0700

    Initial commit

---
+++ Person.php
@@ -0,0 +1,22 @@
+<?php
+require('./Job.php');
+class Person{
+
+  private Object $job;
+
+  public function __construct(){
+    $this->job = new Job();
+    $this->job->setSalary(14700);
+  }
+
+  public function getSalary(): int
+  {
+    return $this->job->getSalary();
+  }
+}
+$person = new Person();
+$person->getSalary();
+
+$salary = $person->getSalary();
+print ($salary);
+?>
